4d839d418a875cd8588b0f85da511150bc6c9290
Ian Glockner <ianglockner@gmail.com>
2025-12-08 16:52:59 -0700

    Added a GameState class

---
+++ src/TwentyOne/Models/GameState.cs
@@ -0,0 +1,13 @@
+using TwentyOne.Models.Enums;
+
+namespace TwentyOne.Models
+{
+    public class GameState ()
+    {
+        public Shoe Shoe { get; set; } = new();
+        public Hand DealerHand { get; set; } = new();
+        public List<Player> Players { get; set; } = [];
+        public List<PlayerHandActions> AvailableActions { get; set; } = [];
+        public string StatusMessage { get; set; } = "";
+    }
+}
