38b03fda647ade55b7b61971f9e3bde2f0901c19
Ian Glockner <ianglockner@gmail.com>
2025-12-13 15:36:50 -0700

    Added short strings to map ranks for abbreviated use

--- src/TwentyOne/Services/GameService.cs
+++ src/TwentyOne/Services/GameService.cs
@@ -54,9 +54,26 @@ namespace TwentyOne.Services
             { Rank.Jack, 10 },
             { Rank.Queen, 10 },
             { Rank.King, 10 }
+        };        
+        
+        public static readonly Dictionary<Rank, string> RankShort = new()
+        {
+            { Rank.Ace, "A" },
+            { Rank.Two, "2" },
+            { Rank.Three, "3" },
+            { Rank.Four, "4" },
+            { Rank.Five, "5" },
+            { Rank.Six, "6" },
+            { Rank.Seven, "7" },
+            { Rank.Eight, "8" },
+            { Rank.Nine, "9" },
+            { Rank.Ten, "10" },
+            { Rank.Jack, "J" },
+            { Rank.Queen, "Q" },
+            { Rank.King, "K" }
         };
 
-        public static Dictionary<Suit, string> SuitStrings = new()
+        public static readonly Dictionary<Suit, string> SuitSymbol = new()
         {
             { Suit.Clubs, "♣" },
             { Suit.Diamonds, "♦" },
