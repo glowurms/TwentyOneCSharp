bf6b80b830db48e4729e69ff88eb852044569945
Ian Glockner <ianglockner@gmail.com>
2025-12-30 19:03:19 -0700

    removed available actions from Player class

--- src/TwentyOne/Models/GameState.cs
+++ src/TwentyOne/Models/GameState.cs
@@ -5,7 +5,6 @@ namespace TwentyOne.Models
         public Shoe Shoe { get; set; } = new();
         public Hand DealerHand { get; set; } = new();
         public List<Player> Players { get; set; } = [];
-        public List<PlayerHandActions> AvailableActions { get; set; } = [];
         public GamePhase CurrentGamePhase { get; set; } = GamePhase.Betting;
         public int CurrentPlayerIndex { get; set; } = 0;
         public int CurrentHandIndex { get; set; } = 0;
