31d5b7a1af9fca46d4b0b7bde4bdc72d1b412219
Ian Glockner <ianglockner@gmail.com>
2026-01-02 15:39:50 -0700

    Moved RulesServices tests to separate tests file and added newly publicly available tests for Split and DoubleDown

--- tests/TwentyOne.Tests/Services/GameService.Test.cs
+++ tests/TwentyOne.Tests/Services/GameService.Test.cs
@@ -2,187 +2,61 @@ using TwentyOne.Constants;
 using TwentyOne.Models;
 using TwentyOne.Services;
 
-namespace TwentyOne.Tests.Services
+namespace TwentyOne.Tests.Services;
+public class GameServiceTest
 {
-    public class GameServiceTest
+    [Theory]
+    [InlineData(-1, 500, 6)]
+    [InlineData(0, 300, 4)]
+    [InlineData(1, 200, 8)]
+    [InlineData(2, 500, 6)]
+    [InlineData(6, 300, 4)]
+    [InlineData(7, 200, 8)]
+    [InlineData(10, 200, 8)]
+    public void StartNewGame_InitializesGameStateCorrectly(int requestedPlayerCount, int bankRollAmount, int shoeCount)
     {
-        [Theory]
-        [InlineData(-1, 500, 6)]
-        [InlineData(0, 300, 4)]
-        [InlineData(1, 200, 8)]
-        [InlineData(2, 500, 6)]
-        [InlineData(6, 300, 4)]
-        [InlineData(7, 200, 8)]
-        [InlineData(10, 200, 8)]
-        public void StartNewGame_InitializesGameStateCorrectly(int requestedPlayerCount, int bankRollAmount, int shoeCount)
+        GameService gameService = new();
+        GameState gameState = gameService.StartNewGame(requestedPlayerCount, bankRollAmount, shoeCount);
+
+        int expectedPlayerCount = Math.Min(GameConstants.MaxPlayers, Math.Max(GameConstants.MinPlayers, requestedPlayerCount));
+
+        Assert.Equal(shoeCount, gameState.Shoe.DeckCount);
+        Assert.Equal(expectedPlayerCount, gameState.Players.Count);
+        Assert.Equal(GamePhase.Betting, gameState.CurrentGamePhase);
+
+        foreach(Player player in gameState.Players)
         {
-            GameService gameService = new();
-            GameState gameState = gameService.StartNewGame(requestedPlayerCount, bankRollAmount, shoeCount);
+            Assert.Equal(bankRollAmount, player.Bankroll);
+        }
+    }
 
-            int expectedPlayerCount = Math.Min(GameConstants.MaxPlayers, Math.Max(GameConstants.MinPlayers, requestedPlayerCount));
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(10)]
+    public void DealInitialCards_WorksCorrectly(int requestedPlayerCount)
+    {
+        GameService gameService = new();
+        GameState gameState = gameService.StartNewGame(requestedPlayerCount, 500, 6);
 
-            Assert.Equal(shoeCount, gameState.Shoe.DeckCount);
-            Assert.Equal(expectedPlayerCount, gameState.Players.Count);
-            Assert.Equal(GamePhase.Betting, gameState.CurrentGamePhase);
+        gameService.DealInitialCards();
 
-            foreach(Player player in gameState.Players)
+        int cardsInPlayCount = (gameState.Players.Count * 2) + 2; // 2 Cards to each player and the dealer
+
+        Assert.Equal(2, gameState.DealerHand.TotalCardCount);
+
+        foreach(Player player in gameState.Players)
+        {
+            foreach(Hand hand in player.HandsInPlay)
             {
-                Assert.Equal(bankRollAmount, player.Bankroll);
+                Assert.Equal(2, hand.TotalCardCount);
             }
         }
-
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(6)]
-        [InlineData(7)]
-        [InlineData(10)]
-        public void DealInitialCards_WorksCorrectly(int requestedPlayerCount)
-        {
-            GameService gameService = new();
-            GameState gameState = gameService.StartNewGame(requestedPlayerCount, 500, 6);
-
-            gameService.DealInitialCards();
-
-            int cardsInPlayCount = (gameState.Players.Count * 2) + 2; // 2 Cards to each player and the dealer
-
-            Assert.Equal(2, gameState.DealerHand.TotalCardCount);
-
-            foreach(Player player in gameState.Players)
-            {
-                foreach(Hand hand in player.HandsInPlay)
-                {
-                    Assert.Equal(2, hand.TotalCardCount);
-                }
-            }
-            // Each player and the dealer has 2 cards, so total 4 cards dealt
-            Assert.Equal(gameState.Shoe.TotalCardCount - cardsInPlayCount, gameState.Shoe.UndealtCardCount);
-        }
-
-        [Theory]
-        // General card value scenarios
-        [InlineData(new Rank[] { Rank.Two, Rank.Three }, 5)]
-        [InlineData(new Rank[] { Rank.Two, Rank.Three, Rank.Four }, 9)]
-        [InlineData(new Rank[] { Rank.Two, Rank.Three, Rank.Four, Rank.Five }, 14)]
-        [InlineData(new Rank[] { Rank.Five, Rank.Six, Rank.Ten }, 21)]
-        // Ace scenarios
-        [InlineData(new Rank[] { Rank.Ace, Rank.Five }, 16)]
-        [InlineData(new Rank[] { Rank.Ace, Rank.Ten }, 21)]
-        [InlineData(new Rank[] { Rank.Ace, Rank.Jack }, 21)]
-        [InlineData(new Rank[] { Rank.Ace, Rank.Queen }, 21)]
-        [InlineData(new Rank[] { Rank.Ace, Rank.King }, 21)]
-        // Mutiple Ace scenarios
-        [InlineData(new Rank[] { Rank.Ace, Rank.Ace, Rank.Nine }, 21)]
-        [InlineData(new Rank[] { Rank.Ace, Rank.Ace, Rank.Ace }, 13)]
-        // 7 Ace scenario (unlikely but possible in testing)
-        [InlineData(new Rank[] { 
-            Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
-            Rank.Ace, Rank.Ace }, 17)]
-        // 11 Ace scenario (unlikely but possible in testing)
-        [InlineData(new Rank[] { 
-            Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
-            Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
-            Rank.Ace }, 21)]
-        // 12 Ace scenario (unlikely but possible in testing)
-        [InlineData(new Rank[] { 
-            Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
-            Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
-            Rank.Ace, Rank.Ace }, 12)]
-        public void HandValue_CalculatesCorrectly(Rank[] ranks, int expectedValue)
-        {
-            Hand hand = new();
-            foreach (var rank in ranks)
-            {
-                hand.AddCard(new Card(rank, Suit.Hearts));
-            }
-
-            int value = RulesService.HandValue(hand);
-
-            Assert.Equal(expectedValue, value);
-        }
-
-        [Theory]
-        [InlineData(new Rank[] { Rank.Ten, Rank.King, Rank.Two }, true)]
-        [InlineData(new Rank[] { Rank.Ten, Rank.King }, false)]
-        public void HandIsBust_WorksCorrectly(Rank[] ranks, bool expectedIsBust)
-        {
-            Hand hand = new();
-            foreach (var rank in ranks)
-            {
-                hand.AddCard(new Card(rank, Suit.Hearts));
-            }
-
-            bool isBust = RulesService.HandIsBust(hand);
-
-            Assert.Equal(expectedIsBust, isBust);
-        }
-
-        [Theory]
-        [InlineData(new Rank[] { Rank.Ten, Rank.King, Rank.Ace }, true)]
-        [InlineData(new Rank[] { Rank.Ten, Rank.King }, false)]
-        // 11 Ace scenario (unlikely but possible in testing)
-        [InlineData(new Rank[] { 
-            Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
-            Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
-            Rank.Ace }, true)]
-        public void HandIsTwentyOne_WorksCorrectly(Rank[] ranks, bool expectedIsTwentyOne)
-        {
-            Hand hand = new();
-            foreach (var rank in ranks)
-            {
-                hand.AddCard(new Card(rank, Suit.Hearts));
-            }
-
-            bool isTwentyOne = RulesService.HandIsTwentyOne(hand);
-
-            Assert.Equal(expectedIsTwentyOne, isTwentyOne);
-        }
-
-        [Theory]
-        [InlineData(new Rank[] { Rank.Ace, Rank.King }, true)]  // Natural 21
-        [InlineData(new Rank[] { Rank.Ten, Rank.King }, false)] // Not 21
-        [InlineData(new Rank[] { Rank.Ace, Rank.Nine, Rank.Ace }, false)] // 21 but not natural
-        public void HandIsNatural_WorksCorrectly(Rank[] ranks, bool expectedIsNatural)
-        {
-            Hand hand = new();
-            foreach (var rank in ranks)
-            {
-                hand.AddCard(new Card(rank, Suit.Hearts));
-            }
-
-            bool isNatural = RulesService.HandIsNatural(hand);
-
-            Assert.Equal(expectedIsNatural, isNatural);
-        }
-
-        // [Theory]
-        // [InlineData(new Rank[] { Rank.Two, Rank.Three }, true)] // 5 < 17
-        // [InlineData(new Rank[] { Rank.Ten, Rank.Seven }, false)] // 17 == 17
-        // [InlineData(new Rank[] { Rank.King, Rank.Nine }, false)] // 19 > 17
-        // public void DealerShouldDraw_WorksCorrectly(Rank[] ranks, bool expectedShouldDraw)
-        // {
-        //     Hand dealerHand = new();
-        //     foreach (var rank in ranks)
-        //     {
-        //         dealerHand.AddCard(new Card(rank, Suit.Hearts));
-        //     }
-
-        //     GameState gameState = new()
-        //     {
-        //         Shoe = new Shoe(3),
-        //         DealerHand = new Hand(),
-        //         Players = [new Player("Player1", 100m)]
-        //     };
-
-        //     gameState.DealerHand = dealerHand;
-
-        //     GameService gameService = new(ref gameState);
-
-        //     bool shouldDraw = gameService.DealerShouldDraw();
-
-        //     Assert.Equal(expectedShouldDraw, shouldDraw);
-        // }
+        // Each player and the dealer has 2 cards, so total 4 cards dealt
+        Assert.Equal(gameState.Shoe.TotalCardCount - cardsInPlayCount, gameState.Shoe.UndealtCardCount);
     }
 }
---
+++ tests/TwentyOne.Tests/Services/RulesService.Test.cs
@@ -0,0 +1,175 @@
+using TwentyOne.Constants;
+using TwentyOne.Models;
+using TwentyOne.Services;
+
+namespace TwentyOne.Tests.Services;
+
+public class RulesServiceTest
+{
+    [Theory]
+    // General card value scenarios
+    [InlineData(new Rank[] { Rank.Two, Rank.Three }, 5)]
+    [InlineData(new Rank[] { Rank.Two, Rank.Three, Rank.Four }, 9)]
+    [InlineData(new Rank[] { Rank.Two, Rank.Three, Rank.Four, Rank.Five }, 14)]
+    [InlineData(new Rank[] { Rank.Five, Rank.Six, Rank.Ten }, 21)]
+    // Ace scenarios
+    [InlineData(new Rank[] { Rank.Ace, Rank.Five }, 16)]
+    [InlineData(new Rank[] { Rank.Ace, Rank.Ten }, 21)]
+    [InlineData(new Rank[] { Rank.Ace, Rank.Jack }, 21)]
+    [InlineData(new Rank[] { Rank.Ace, Rank.Queen }, 21)]
+    [InlineData(new Rank[] { Rank.Ace, Rank.King }, 21)]
+    // Mutiple Ace scenarios
+    [InlineData(new Rank[] { Rank.Ace, Rank.Ace, Rank.Nine }, 21)]
+    [InlineData(new Rank[] { Rank.Ace, Rank.Ace, Rank.Ace }, 13)]
+    // 7 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
+        Rank.Ace, Rank.Ace }, 17)]
+    // 11 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
+        Rank.Ace }, 21)]
+    // 12 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
+        Rank.Ace, Rank.Ace }, 12)]
+    public void HandValue_CalculatesCorrectly(Rank[] ranks, int expectedValue)
+    {
+        Hand hand = new();
+        foreach (var rank in ranks)
+        {
+            hand.AddCard(new Card(rank, Suit.Hearts));
+        }
+
+        int value = RulesService.HandValue(hand);
+
+        Assert.Equal(expectedValue, value);
+    }
+
+    [Theory]
+    [InlineData(new Rank[] { Rank.Ten, Rank.King, Rank.Two }, true)]
+    [InlineData(new Rank[] { Rank.Ten, Rank.King }, false)]
+    // (21) 11 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
+        Rank.Ace }, false)]
+    // (12) 12 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
+        Rank.Ace, Rank.Ace }, false)]
+    public void HandIsBust_WorksCorrectly(Rank[] ranks, bool expectedIsBust)
+    {
+        Hand hand = new();
+        foreach (var rank in ranks)
+        {
+            hand.AddCard(new Card(rank, Suit.Hearts));
+        }
+
+        bool isBust = RulesService.HandIsBust(hand);
+
+        Assert.Equal(expectedIsBust, isBust);
+    }
+
+    [Theory]
+    [InlineData(new Rank[] { Rank.Ten, Rank.King, Rank.Ace }, true)]
+    [InlineData(new Rank[] { Rank.Ten, Rank.King }, false)]
+    // 11 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace,
+        Rank.Ace }, true)]
+    public void HandIsTwentyOne_WorksCorrectly(Rank[] ranks, bool expectedIsTwentyOne)
+    {
+        Hand hand = new();
+        foreach (var rank in ranks)
+        {
+            hand.AddCard(new Card(rank, Suit.Hearts));
+        }
+
+        bool isTwentyOne = RulesService.HandIsTwentyOne(hand);
+
+        Assert.Equal(expectedIsTwentyOne, isTwentyOne);
+    }
+
+    [Theory]
+    [InlineData(new Rank[] { Rank.Ace, Rank.King }, true)]  // Natural 21
+    [InlineData(new Rank[] { Rank.Ten, Rank.King }, false)] // Not 21
+    [InlineData(new Rank[] { Rank.Ace, Rank.Nine, Rank.Ace }, false)] // 21 but not natural
+    public void HandIsNatural_WorksCorrectly(Rank[] ranks, bool expectedIsNatural)
+    {
+        Hand hand = new();
+        foreach (var rank in ranks)
+        {
+            hand.AddCard(new Card(rank, Suit.Hearts));
+        }
+
+        Assert.Equal(expectedIsNatural, RulesService.HandIsNatural(hand));
+    }
+
+    [Theory]
+    // 2 cards Value is 9, 10, or 11
+    [InlineData(new Rank[] { Rank.Three, Rank.Six }, true)]
+    [InlineData(new Rank[] { Rank.Five, Rank.Six }, true)]
+    // TODO: Get this to pass even though you really wouldn't
+    // [InlineData(new Rank[] { Rank.Ace, Rank.King }, true)]
+    // Value not 9, 10, or 11
+    [InlineData(new Rank[] { Rank.Ten, Rank.King }, false)]
+    // Hand is not 2 cards
+    [InlineData(new Rank[] { Rank.Ace, Rank.Nine, Rank.Ace }, false)]
+    public void CanDoubleDown_WorksCorrectly(Rank[] ranks, bool expectedCanDoubleDown)
+    {
+        Hand hand = new();
+        foreach (var rank in ranks)
+        {
+            hand.AddCard(new Card(rank, Suit.Hearts));
+        }
+
+        Assert.Equal(expectedCanDoubleDown, RulesService.CanDoubleDown(hand));
+        
+    }
+
+    [Theory]
+    [InlineData(new Rank[] { Rank.Ace, Rank.Ace }, 1, true)]
+    [InlineData(new Rank[] { Rank.Ten, Rank.Ten }, 3, true)]
+    [InlineData(new Rank[] { Rank.Ten, Rank.Ten }, 4, false)]
+    [InlineData(new Rank[] { Rank.Ten, Rank.Ten }, 5, false)]
+    public void CanSplitHand_WorksCorrectly(Rank[] ranks, int handCount, bool expectedCanSplitValue)
+    {
+        Hand hand = new();
+        foreach (var rank in ranks)
+        {
+            hand.AddCard(new Card(rank, Suit.Hearts));
+        }
+
+        Assert.Equal(expectedCanSplitValue, RulesService.CanSplitHand(hand, handCount));
+    }
+
+
+    [Theory]
+    [InlineData(new Rank[] { Rank.Two, Rank.Three }, true)] // 5 < 17
+    [InlineData(new Rank[] { Rank.Ten, Rank.Seven }, false)] // 17 == 17
+    [InlineData(new Rank[] { Rank.King, Rank.Nine }, false)] // 19 > 17
+    [InlineData(new Rank[] { Rank.Ace, Rank.Nine, Rank.Ace }, false)] // 21 > 17 Three cards
+    // (16) 6 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
+        Rank.Ace, Rank.Ace }, true)]
+    // (17) 7 Ace scenario (unlikely but possible in testing)
+    [InlineData(new Rank[] { 
+        Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, Rank.Ace, 
+        Rank.Ace, Rank.Ace }, false)]
+    public void DealerShouldDraw_WorksCorrectly(Rank[] ranks, bool expectedShouldDraw)
+    {
+        Hand dealerHand = new();
+        foreach (var rank in ranks)
+        {
+            dealerHand.AddCard(new Card(rank, Suit.Hearts));
+        }
+
+        Assert.Equal(expectedShouldDraw, RulesService.DealerShouldDraw(dealerHand));
+    }
+}
