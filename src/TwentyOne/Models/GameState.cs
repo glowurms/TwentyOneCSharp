2f9ff3a480b91dbf7991da825ce318f9658183ec
Ian Glockner <ianglockner@gmail.com>
2026-01-02 14:48:19 -0700

    Added members to store the DealerAction and available player actions

--- src/TwentyOne/Models/GameState.cs
+++ src/TwentyOne/Models/GameState.cs
@@ -4,9 +4,11 @@ namespace TwentyOne.Models
     {
         public Shoe Shoe { get; set; } = new();
         public Hand DealerHand { get; set; } = new();
+        public DealerActions DealerAction { get; set; } = DealerActions.None;
         public List<Player> Players { get; set; } = [];
         public GamePhase CurrentGamePhase { get; set; } = GamePhase.Betting;
         public int CurrentPlayerIndex { get; set; } = 0;
         public int CurrentHandIndex { get; set; } = 0;
+        public List<PlayerActions> CurrentPlayerOptions { get; set; } = [PlayerActions.None];
     }
 }
