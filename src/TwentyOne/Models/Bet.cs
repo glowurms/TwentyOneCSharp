d6ef14f36dab7ff8c0b261096f2f0cb0fe25abda
Ian Glockner <ianglockner@gmail.com>
2026-01-06 10:27:39 -0700

    Added BetType and BetResolutionType members to Bet class

--- src/TwentyOne/Models/Bet.cs
+++ src/TwentyOne/Models/Bet.cs
@@ -1,8 +1,10 @@
 namespace TwentyOne.Models;
 
-public class Bet(Player targetPlayer, Hand targetHand, decimal betAmount)
+public class Bet(Player targetPlayer, Hand targetHand, decimal betAmount, BetType betType)
 {
     public readonly Player Player = targetPlayer;
     public readonly Hand Hand = targetHand;
-    public decimal Amount = betAmount;
+    public readonly decimal Amount = betAmount;
+    public readonly BetType Type = betType;
+    public BetResolutionType Resolution = BetResolutionType.None;
 }
