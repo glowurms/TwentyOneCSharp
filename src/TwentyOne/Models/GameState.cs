10378bc34f27acfeb89f4f96783c14f647034cb8
Ian Glockner <ianglockner@gmail.com>
2026-01-06 10:29:13 -0700

    Changed member ordering into related groups and added members to track various states last step member values

--- src/TwentyOne/Models/GameState.cs
+++ src/TwentyOne/Models/GameState.cs
@@ -4,14 +4,25 @@ namespace TwentyOne.Models
     {
         public Shoe Shoe { get; set; } = new();
         public GamePhase GamePhase { get; set; } = GamePhase.Betting;
+        public decimal TableWinnings = 0m; // Unimportant, but interesting stat
+
         public Hand DealerHand { get; set; } = new();
         public DealerActions DealerAction { get; set; } = DealerActions.None;
+
         public List<Player> Players { get; set; } = [];
         public int CurrentPlayerIndex { get; set; } = 0;
         public int CurrentHandIndex { get; set; } = 0;
+
         public PlayerActions CurrentPlayerIntent { get; set; } = PlayerActions.None;
         public List<PlayerActions> CurrentPlayerOptions { get; set; } = [PlayerActions.None];
+
         public List<Bet> ActiveBets = [];
-        public decimal TableWinnings = 0m; // Unimportant, but interesting stat
+
+        public DealerActions? LastDealerAction = null;
+        public Player? LastPlayer = null;
+        public PlayerActions? LastPlayerIntent = null;
+        public Hand? LastHand = null;
+        public Card? LastDrawnCard = null;
+        public Bet? LastResolvedBet = null;
     }
 }
