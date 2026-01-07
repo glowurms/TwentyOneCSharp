17e669fa5ed990e3241c783a50c8e4d7d57a3610
Ian Glockner <ianglockner@gmail.com>
2026-01-07 11:35:41 -0700

    Added enum GamePhaseStage to track GamePhase flow

--- src/TwentyOne/Models/Enums.cs
+++ src/TwentyOne/Models/Enums.cs
@@ -1,47 +1,45 @@
-namespace TwentyOne.Models
+namespace TwentyOne.Models;
+public enum Suit
 {
-    public enum Suit
-    {
-        Clubs, Diamonds, Hearts, Spades
-    }
+    Clubs, Diamonds, Hearts, Spades
+}
 
-    public enum Rank
-    {
-        Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
-    }
+public enum Rank
+{
+    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+}
 
-    public enum GamePhase
-    {
-        Betting,
-        Dealing,
-        Naturals,
-        PlayerTurns,
-        DealerTurn,
-        RoundEnd
-    }
+public enum GamePhase
+{
+    Betting, Dealing, Naturals, PlayerTurns, DealerTurn, RoundEnd
+}
 
-    public enum DealerActions
-    {
-       ShowFaceDown, Draw, Stand, None
-    }
+public enum GamePhaseStage
+{
+    Begin, InProgress, End
+}
 
-    public enum PlayerActions
-    {
-       Bet, Stand, Hit, DoubleDown, Split, None
-    }
+public enum DealerActions
+{
+    ShowFaceDown, Draw, Stand, None
+}
 
-    public enum GameActions
-    {
-        Continue, Cancel, Instructions, Quit
-    }
+public enum PlayerActions
+{
+    Bet, Stand, Hit, DoubleDown, Split, None
+}
 
-    public enum BetType
-    {
-        Normal, DoubleDown, Split
-    }
+public enum GameActions
+{
+    Continue, Cancel, Instructions, Quit
+}
 
-    public enum BetResolutionType
-    {
-        Win, Lose, Standoff, Busted, DoubleDownWin, None
-    }
+public enum BetType
+{
+    Normal, DoubleDown, Split
+}
+
+public enum BetResolutionType
+{
+    Win, Lose, Standoff, Busted, DoubleDownWin, None
 }
